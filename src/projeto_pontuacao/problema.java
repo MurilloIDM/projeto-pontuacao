6d98537b7bd8a2e86492c1c3ef181522679f2a66
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-15 19:18:28 -0300

    Leitura dos outros dados

--- src/projeto_pontuacao/problema.java
+++ src/projeto_pontuacao/problema.java
@@ -10,8 +10,9 @@ public class problema {
 		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 		
-		int totalCompras;
+		int totalCompras, inadimplencia;
 		double ticketMedio;
+		char formaPagamento;
 		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
@@ -21,6 +22,11 @@ public class problema {
 		totalCompras = input.nextInt();
 		System.out.print("Qual o ticket médio? ");
 		ticketMedio = input.nextDouble();
+		
+		System.out.print("\nQuantas vezes o cliente atrasou o pagamento? ");
+		inadimplencia = input.nextInt();
+		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
+		formaPagamento = input.next().charAt(0);
 		
 		input.close();
 
