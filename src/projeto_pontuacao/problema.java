344728656186e3a36f3220d10d27dbbf736d38c9
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-15 19:27:30 -0300

    Score de volume de compras

--- src/projeto_pontuacao/problema.java
+++ src/projeto_pontuacao/problema.java
@@ -10,9 +10,11 @@ public class problema {
 		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 		
-		int totalCompras, inadimplencia;
+		int totalCompras, inadimplencia, scoreCompras;
 		double ticketMedio;
 		char formaPagamento;
+		
+		scoreCompras = 0;
 		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
@@ -27,6 +29,18 @@ public class problema {
 		inadimplencia = input.nextInt();
 		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
 		formaPagamento = input.next().charAt(0);
+		
+		if (totalCompras == 0) {
+			scoreCompras = 0;
+		} else if ((ticketMedio > 0 && ticketMedio <= 3000) && totalCompras >= 1 && totalCompras <= 2) {
+			scoreCompras = 20;
+		} else if ((ticketMedio > 0 && ticketMedio <= 3000) && totalCompras > 2) {
+			scoreCompras = 40;
+		} else if (ticketMedio > 3000) {
+			scoreCompras = 60;
+		}
+		
+		System.out.printf("%nScore de volume de compras = %d pontos%n", scoreCompras);
 		
 		input.close();
 
