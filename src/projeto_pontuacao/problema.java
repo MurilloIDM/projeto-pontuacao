fbda706e82c45b76dce932eeb35cc75a4ef4eb5a
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-15 19:40:27 -0300

    Scores de inadimplencia e pagamento

--- src/projeto_pontuacao/problema.java
+++ src/projeto_pontuacao/problema.java
@@ -10,11 +10,13 @@ public class problema {
 		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 		
-		int totalCompras, inadimplencia, scoreCompras;
+		int totalCompras, inadimplencia, scoreCompras, scoreAtraso, scorePagamento;
 		double ticketMedio;
 		char formaPagamento;
 		
 		scoreCompras = 0;
+		scoreAtraso = 0;
+		scorePagamento = 0;
 		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
@@ -30,6 +32,7 @@ public class problema {
 		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
 		formaPagamento = input.next().charAt(0);
 		
+		//Score Compras
 		if (totalCompras == 0) {
 			scoreCompras = 0;
 		} else if ((ticketMedio > 0 && ticketMedio <= 3000) && totalCompras >= 1 && totalCompras <= 2) {
@@ -40,7 +43,28 @@ public class problema {
 			scoreCompras = 60;
 		}
 		
+		
+		//Score Inadimplência
+		if (inadimplencia > 1 || totalCompras == 0) {
+			scoreAtraso = 0;
+		} else if (totalCompras > 0 && (inadimplencia > 0 && inadimplencia <= 1)) {
+			scoreAtraso = 15;
+		} else if (totalCompras > 0 && inadimplencia == 0) {
+			scoreAtraso = 30;
+		}
+		
+		//Score Pagamento
+		if ((formaPagamento == 'C' || formaPagamento == 'c') || (formaPagamento == 'B' || formaPagamento == 'b')) {
+			scorePagamento = 10;
+		} else {
+			scorePagamento = 5;
+		}
+		
+		
 		System.out.printf("%nScore de volume de compras = %d pontos%n", scoreCompras);
+		
+		System.out.printf("%nScore de inadimplência = %d pontos%n", scoreAtraso);
+		System.out.printf("Score de forma de pagamento = %d%n", scorePagamento);
 		
 		input.close();
 
