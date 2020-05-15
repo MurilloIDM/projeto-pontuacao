f50c0b85bc49c62877881a593a7ea1d2d9010574
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-15 19:14:51 -0300

    Leitura do volume

--- src/projeto_pontuacao/problema.java
+++ src/projeto_pontuacao/problema.java
@@ -10,9 +10,17 @@ public class problema {
 		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
 		
+		int totalCompras;
+		double ticketMedio;
+		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		System.out.print("\nQuantas compras o cliente fez no último ano? ");
+		totalCompras = input.nextInt();
+		System.out.print("Qual o ticket médio? ");
+		ticketMedio = input.nextDouble();
 		
 		input.close();
 
