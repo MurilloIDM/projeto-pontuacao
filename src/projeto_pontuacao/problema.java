7c9d95e7b09086aede8019aa1412c41caed28d8e
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-15 19:11:38 -0300

    Cabecalho

---
+++ src/projeto_pontuacao/problema.java
@@ -0,0 +1,21 @@
+package projeto_pontuacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class problema {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		input.close();
+
+	}
+
+}
